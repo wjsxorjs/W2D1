89c72f8ae85f91f55a70f2e48ca686d098c95167
wjsxorjs <wjsxorjs@naver.com>
2024-04-12 16:48:40 +0900

    JAVA 2024.04.12 0.85

--- Ex20240412/firstTime.sql
+++ Ex20240412/firstTime.sql
@@ -68,5 +68,96 @@ FROM emp
 WHERE job = 'SALESMAN' and (hiredate >= '1981-01-01' and hiredate < '1982-01-01')
 ;
 
+-- 정렬(ORDER BY)
+--   특정 컬럼을 중심으로 오름차순 또는 내림차순으로 정렬을 시켜 출력하고자 할 때 사용한다.
+--   예) emp테이블에서 입사일이 1981년인 사원들의 정보를 사번, 이름, 입사일 순으로 출력하자.
+--      이때, 사번이 높은 것부터 내림차순으로 정렬하여 출력한다.
+
+SELECT empno, ename, hiredate
+FROM emp
+WHERE hiredate >= '1981-01-01' AND hiredate < '1982-01-01'
+ORDER BY empno DESC
+;
+
+
+-- 문) emp테이블에서 직종이 영업사원 또는 관리자인 사원들의 정보를
+--     사번, 이름, 직종, 입사일 순으로 출력하시오.
+--     입사일이 가장 최근인 사원부터 출력
+
+SELECT empno, ename, job, hiredate
+FROM emp
+WHERE job = 'SALESMAN' OR job = 'MANAGER'
+ORDER BY hiredate DESC
+;
+
+
+
+-- 문) emp테이블에서 직종이 영업사원이면서 급여가 1500 미만인 사원들의 정보를
+--     사번, 이름, 직종, 입사일, 급여 순으로 출력하시오.
+
+SELECT empno, ename, job, hiredate, sal
+FROM emp
+WHERE job = 'SALESMAN' AND sal < 1500
+;
+
+
+-- SQL연산자
+--     1. BETWEEN 연산자
+--     2. IN 연산자
+--     3. LIKE 연산자
+
+
+--     1. BETWEEN 연산자
+--        특정 값 A에서 특정값 B의 사이(범위)의 정보를 얻을 때 사용한다.
+
+-- 		예) emp테이블에서 1981년도에 입사한 사원들의 정보를
+-- 			사번, 이름, 직종, 입사일 순으로 출력하자.
+SELECT empno, ename, job, hiredate
+FROM emp
+WHERE hiredate BETWEEN '1981-01-01' AND '1981-12-31' -- = WHERE hiredate >= '1981-01-01' AND hiredate <= '1981-12-31'
+;
+
+-- 		문) emp테이블에서 급여가 1500에서 2000 사이인 사원들의 정보를
+-- 			사번, 이름, 직종, 급여 순으로 출력하자.
+
+SELECT empno, ename, job, sal
+FROM emp
+WHERE sal BETWEEN 1500 AND 2000
+;
+
+--     2. IN 연산자
+--        특정 목록의 결과값에서 비교값이 속하는지를 가려내는 데 사용한다.
+--        [구성]
+--        WHERE [컬럼명] IN (값1, 값2, ... , 값n)
+
+-- 		예) emp테이블에서 급여가 800, 1000, 1200, 1500인 사원들의 정보를
+-- 			사번, 이름, 직종, 급여 순으로 출력하자.
+SELECT empno, ename, job, sal
+FROM emp
+WHERE sal IN (800,1000,1200,1500) -- = WHERE sal = 800 OR sal = 1000 OR sal = 1200 OR sal = 1500
+;
+
+-- 		문) emp테이블에서 직종이 사무직 또는 분석가들의 정보를
+-- 			사번, 이름, 직종, 급여 순으로 출력하자. 단, IN연산자 사용
+
+SELECT empno, ename, job, sal
+FROM emp
+WHERE job IN ('CLERK','ANALYST')
+;
+
+-- 		문) emp테이블에서 직종이 사무직 또는 분석가가 아닌 사원들의 정보를
+-- 			사번, 이름, 직종, 급여 순으로 출력하자. 단, IN연산자 사용
+
+SELECT empno, ename, job, sal
+FROM emp
+WHERE job NOT IN ('CLERK','ANALYST')
+;
+
+-- 		emp테이블에서 보너스(comm)가 null이 아닌 사원들 출력
+SELECT *
+FROM emp
+WHERE comm IS NOT NULL
+;
+
 
 
